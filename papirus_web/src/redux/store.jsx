6beabc6c798314547d2f54722286ba1fdea16f53
Kaan Hançer <kaannhancer@gmail.com>
2025-05-12 23:52:57 +0300

    Store commit

--- papirus_web/src/redux/store.jsx
+++ papirus_web/src/redux/store.jsx
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import loginReducer from '../pages/Login/loginSlice'
+import registerReducer from '../pages/Register/registerSlice'
+import homeReducer from '../pages/Home/Store/homeSlice'
+import cardReducer from '../pages/Card/Store/cardSlice'
+import updateReducer from '../pages/Profile/Store/profileSlice'
 
 export const store = configureStore({
     reducer: {
         login: loginReducer,
+        register: registerReducer,
+        home: homeReducer,
+        card: cardReducer,
+        update: updateReducer,
     },
 })
--- papirus_web/src/utils/axios.jsx
+++ papirus_web/src/utils/axios.jsx
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
+// axios.defaults.headers.common = {
+//     'ngrok-skip-browser-warning': true
+// }
+
 const axiosInstance = axios.create({
     baseURL: 'https://bookstore-backend-8bbr.onrender.com',
+    // baseURL: 'https://9f8e-178-250-91-6.ngrok-free.app'
 })
+
 export default axiosInstance
