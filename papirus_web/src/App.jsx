e1bc35b5e2f7adc126b852a1432c89c3fd590565
Kaan Hançer <kaannhancer@gmail.com>
2025-05-21 11:31:49 +0300

    Empty commit

--- papirus_web/src/App.jsx
+++ papirus_web/src/App.jsx
@@ -1,23 +1,52 @@
 
-import Sidebar from './components/Sidebar';
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Topbar from './components/Topbar';
-import BookItem from './pages/Home/components/BookItem';
+import Profile from './pages/Profile/Profile';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register'
+import CardPage from './pages/Card/CardPage';
+import BookItem from './pages/Home/components/BookItem'
+import { ProtectedRoute } from './components/ProtectedRoute';
+import HomePage from './pages/Home/HomePage'
+import { Card } from '@mui/material';
 
-
+// function LayoutWithCardPage() {
+//   return (
+//     <div>
+//       <Sidebar />
+//       <Profile />
+//     </div>
+//   )
+// }
 
 function App() {
 
 
   return (
-    <div>
-      <Sidebar />
-      <Topbar />
-      <BookItem />
+    <BrowserRouter>
+      {/* Giris yapmamis kullanicilar */}
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
 
-    </div>
+        {/* Giris yapilinca gosterilecek sayfa */}
+        <Route element={<ProtectedRoute />}>
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/card' element={<CardPage />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <div>
+
+    //   <Sidebar />
+    //   <CardPage />
+
+    //   {/* <Topbar />
+    //   <BookItem /> */}
+
+    // </div>
     // <BrowserRouter>
     //   <Routes>
     //     <Route path='/' element={<Login />} />
---
+++ papirus_web/src/pages/Register/Styled.js
@@ -0,0 +1,5 @@
+import { Box, styled } from "@mui/material";
+
+const Wrapper = styled(Box)((({
+    display: 'flex',
+}))) 
