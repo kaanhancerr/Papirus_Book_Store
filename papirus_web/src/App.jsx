7ac10f2254b2e954e52b97a77040be7ef986fadb
Kaan Hançer <kaannhancer@gmail.com>
2025-06-25 23:02:49 +0300

    ThemeProvider

--- papirus_web/src/App.jsx
+++ papirus_web/src/App.jsx
@@ -5,10 +5,11 @@ import Profile from './pages/Profile/Profile';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register'
 import CardPage from './pages/Card/CardPage';
-import BookItem from './pages/Home/components/BookItem'
 import { ProtectedRoute } from './components/ProtectedRoute';
 import HomePage from './pages/Home/HomePage'
-import { Card } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import getTheme from './Theme/AppTheme';
+import { useSelector } from 'react-redux';
 
 // function LayoutWithCardPage() {
 //   return (
@@ -20,24 +21,28 @@ import { Card } from '@mui/material';
 // }
 
 function App() {
+  const mode = useSelector((state) => state.theme.mode)
+  const theme = getTheme(mode);
 
 
   return (
-    <BrowserRouter>
-      {/* Giris yapmamis kullanicilar */}
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        {/* Giris yapmamis kullanicilar */}
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
 
-        {/* Giris yapilinca gosterilecek sayfa */}
-        <Route element={<ProtectedRoute />}>
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/card' element={<CardPage />}></Route>
-          <Route path='/profile' element={<Profile />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* Giris yapilinca gosterilecek sayfa */}
+          <Route element={<ProtectedRoute />}>
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/card' element={<CardPage />}></Route>
+            <Route path='/profile' element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
     // <div>
 
     //   <Sidebar />
